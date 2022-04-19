24b79d03f81cf05e37e691e4376de011edef0bdd
vivek <vivek.koyani@gmail.com>
2022-04-19 23:15:18 +0530

    add changes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='btnhistory'>History</button>
+      <div className='soltline'>
+        <div className='slot'></div>
+        <div className='slotGray'></div>
+        <div className='slotGray'></div>
+      </div>
+      <div className='verticalsolt'></div>
+      <div className='box'>
+        <div className='underbox'></div>
+        <div className='progressbox'>
+
+        </div>
+        <div className='finishbtnbox'>
+
+        </div>
+      </div>
     </div>
   );
 }
